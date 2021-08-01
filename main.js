7dd5be48ebc61f4b02acc11429b3bbe1d7a5a53f
Nguyen Ngoc Chung Chi <nncchi.fitus@gmail..com>
2021-08-01 07:10:05 +0700

    Simple add and show item program

---
+++ main.js
@@ -0,0 +1,196 @@
+// Items
+// Date.month 0-11
+var i1 = {
+    id: 1,
+    title: "iphoneX",
+    amount: 150,
+    date: new Date (2020, 6, 20),       //20/7
+}
+
+var i2 = {
+    id: 2,
+    title: "Oppo reno4",
+    amount: 90,
+    date: new Date (2020, 4, 16),
+}
+
+var i3 = {
+    id: 3,
+    title: "SamSung Galaxy A51",
+    amount: 80,
+    date: new Date (2020, 2, 25),
+}
+
+var i4 = {
+    id: 4,
+    title: "Oppo reno5",
+    amount: 100,
+    date: new Date(2021, 4, 16),        //16/5
+}
+
+var i5 = {
+    id: 5,
+    title: "SamSung Galaxy J3",
+    amount: 10,
+    date: new Date (2021, 6, 25),
+}
+
+var i6 = {
+    id: 6,
+    title: "Redmi X",
+    amount: 120,
+    date: new Date (2022, 5, 15),
+}
+
+var i7 = {
+    id: 7,
+    title: "SamSung Galaxy J2",
+    amount: 20,
+    date: new Date (2020, 10,23)
+}
+
+var i8 = {
+    id: 7,
+    title: "Samsung A51",
+    amount: 20,
+    date: new Date (2022, 1,23)
+}
+
+var i9 = {
+    id: 7,
+    title: "Samsung A51",
+    amount: 51,
+    date: new Date (2022, 9,23)
+}
+
+var listItems = [i1, i2, i3, i4, i5, i6, i7, i8, i9]
+var listMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
+'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+listYears = [2020, 2021, 2022, 2023]
+
+// Functions
+
+// Load chart
+var loadChart = function(year) {
+
+    // get full items of current year
+    listItemsOfCurrentYear = listItems.filter(function(item) {
+        return item.date.getFullYear() == year;
+    })
+
+    // array 12*0 amounts of months
+    var monthsValue = []
+    for (var i=0; i<12; i++) {
+        monthsValue.push(0)
+    }   
+
+    listItemsOfCurrentYear.forEach(function(item) {
+        monthsValue[item.date.getMonth()] += item.amount;
+    })
+
+    // Get max of months value
+    maxMonthsValue = Math.max(...monthsValue)
+
+    var barChartElement = document.querySelector('.bar-chart')
+    htmls = ''
+    listMonths.forEach(function(month,index) {
+        htmls += `<div class="chart-item" data-label="${month}">
+                    <div class="item-outline">
+                        <div class="item-fill" style="height:${monthsValue[index]/maxMonthsValue*100}%"></div>
+                    </div>
+                </div>`
+    })
+    barChartElement.innerHTML = htmls
+
+}
+
+// Load all items of 1 year
+var loadItems = function(year){
+
+    // Load list item
+    var listItemsElement = document.querySelector('.list-items')
+
+    listItemsOfCurrentYear = listItems.filter(function(item) {
+        return item.date.getFullYear() == year;
+    })
+
+    htmls = ''
+
+    if (!listItemsOfCurrentYear.length) {
+        htmls = `<h2>Not found</h2>`
+    }
+    else {
+        listItemsOfCurrentYear.forEach(function(item) {
+            htmls += `<div class="item-info">
+                        <div class="info-date">
+                            <div class="date-month">${item.date.getMonth() + 1}</div>
+                            <div class="date-year">${item.date.getFullYear()}</div>
+                            <div class="date-day">${item.date.getDate()}</div>
+                        </div>
+                        <label for="item-info" class="info-label">${item.title}</label>
+                        <div class="info-price">
+                            $${item.amount}
+                        </div>
+                    </div>`
+            
+        })
+    
+    }
+
+    listItemsElement.innerHTML = htmls
+}
+
+// Get information of input item
+var handle = function() {
+
+    var titleElement = document.querySelector('input[type="text"]')
+    var amountElement = document.querySelector('input[type="number"]')
+    var dateElement = document.querySelector('input[type="date"]')
+    var formBtnElement = document.querySelector('.form>.form-btn>.btn') 
+    
+    // Handle when click Add Expense
+    formBtnElement.onclick = function() {
+        var titleElement = document.querySelector('.item-title>.rect')
+        var amountElement = document.querySelector('.item-number>.rect')
+        var dateElement = document.querySelector('.item-date>.rect')
+        var selectYearElement = document.querySelector('.year-select')
+
+        var title = titleElement.value
+        var amount = amountElement.value 
+        var date = dateElement.value
+        var item = {
+            title: title,
+            amount: Number(amount),
+            date: new Date(date),
+        }
+
+        listItems.unshift(item)
+
+        // If year current == year input then load chart and all Items
+        if (selectYearElement.value==item.date.getFullYear()) {
+            loadChart(selectYearElement.value)
+            loadItems(selectYearElement.value)
+        }
+    }
+
+    // Load chart and items when select year
+    var selectYearElement = document.querySelector('.year-select')
+    selectYearElement.onchange = function(e) {
+        loadChart(e.target.value)
+        loadItems(e.target.value)
+    }    
+}
+
+// Load default
+function load() {
+    loadChart(2020)
+    loadItems(2020)
+}
+
+var main = function (){
+    load()
+    handle();
+}
+
+main()
